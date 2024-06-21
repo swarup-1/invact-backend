88c04b677049de1b2ed5bdd8901be10a5ad06164
swarup-1 <swarupkadoli9@gmail.com>
2024-06-21 21:51:23 +0530

    .

--- configs/db.js
+++ configs/db.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 
 mongoose.set('strictQuery', true);
 
-// const connections = mongoose.connect(process.env.MONGO_URL);
 const connections = mongoose.connect("mongodb+srv://swarupkadoli9:invact@cluster0.p8ptqyc.mongodb.net/invact?retryWrites=true&w=majority&appName=Cluster0");
 
 module.exports = {
