ba4e4a6335d4014f3dba241e788915ca7c13eba7
swarup-1 <swarupkadoli9@gmail.com>
2024-06-21 21:21:13 +0530

    .

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ const { moviesRouter } = require('./Routes/Movies.routes');
 require('dotenv').config()
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 
-app.use(cors({
-    origin: "*"
-}))
 app.use("/movies", moviesRouter)
 
 
